1bcac30d367577eb8e56963d6b8538f69a29c49a
Jamie Bradley <jamie@endeavour-digital.com>
2020-07-31 17:06:54 +0100

    Linting
    This commit introduces Stylelint for scss linting.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,10 @@
 const sass = require("gulp-sass");
+const reporter = require("postcss-reporter");
+const syntax = require("postcss-scss");
+const stylelint = require("stylelint");
+const postcss = require("gulp-postcss");
+const tildeImporter = require("node-sass-tilde-importer");
+
 const { dest, series, src, watch } = require("gulp");
 
 /**
@@ -11,6 +17,7 @@ function styles(cb) {
 
   const options = {
     outputStyle: process.env.NODE_ENV === "production" ? "compressed" : "nested",
+    importer: tildeImporter,
   };
 
   src(source).pipe(sass(options).on("error", sass.logError)).pipe(dest(target));
@@ -20,13 +27,27 @@ function styles(cb) {
   cb();
 }
 
+function linter(cb) {
+  const source = "resources/scss/**/*.scss";
+
+  const processors = [
+    stylelint(require("./stylelint.config")),
+    reporter({
+      clearReportedMessages: true,
+    }),
+  ];
+
+  src(source).pipe(postcss(processors, { syntax: syntax }));
+
+  cb();
+}
+
 /**
  * Watch files and perform various tasks.
  * We only need to run the scss compilation for now
  */
 function watcher(cb) {
-  watch(["resources/scss/**/*.scss"], series(styles));
-
+  watch(["resources/scss/**/*.scss"], series(styles, linter));
   cb();
 }
 
@@ -35,4 +56,4 @@ function watcher(cb) {
  */
 exports.default = series(styles);
 exports.watcher = watcher;
-exports.styles = styles;
+exports.linter = linter;
